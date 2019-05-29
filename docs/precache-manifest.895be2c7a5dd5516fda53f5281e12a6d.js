d0c3f39b273e447185f2ce6eb1d30152ba82eeac
pbalogh <pbalogh@pbalogh-w00125.local>
2019-05-29 09:44:56 -0400

    Adding docs folder for github page

---
+++ docs/precache-manifest.895be2c7a5dd5516fda53f5281e12a6d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6469a0c2aa42c3584530f881ec8714ab",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1961086d9e93b02cd056",
+    "url": "./static/css/main.1bc61611.chunk.css"
+  },
+  {
+    "revision": "9cc8d3dda45572e9bbbf",
+    "url": "./static/js/2.37447777.chunk.js"
+  },
+  {
+    "revision": "1961086d9e93b02cd056",
+    "url": "./static/js/main.1f25e7cf.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
